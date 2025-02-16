ac160416f4531f72c03139de39c5d7f41f178c7f
pavandhadge <dhadgepavan00@gmail.com>
2025-02-16 20:47:55 +0530

    parser added

---
+++ parser/parse_test.go
@@ -0,0 +1,63 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/pavandhadge/monkeylang/ast"
+	"github.com/pavandhadge/monkeylang/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+	let x=10;
+	let y=5;
+	let foobar = 838383;`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatal("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program statement does not contain 3 statements . got=%d", len(program.Statements))
+	}
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%s", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s is not *ast.LetStatement . got=%T", s)
+		return false
+
+	}
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt>name.Vlaue not %s. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+		return false
+	}
+	return true
+}
---
+++ parser/parser.go
@@ -0,0 +1,51 @@
+package parser
+
+import (
+	"github.com/pavandhadge/monkeylang/ast"
+	"github.com/pavandhadge/monkeylang/lexer"
+	"github.com/pavandhadge/monkeylang/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	currToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l: l,
+	}
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.currToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
