2378d9ccf1877ac1f776660f93d2966b8641956a
Pavan Dhadge <146618777+pavandhadge@users.noreply.github.com>
2025-02-17 04:55:22 +0000

    letstament parsing and error in parser added

--- parser/parser.go
+++ parser/parser.go
@@ -10,11 +10,13 @@ type Parser struct {
 	l         *lexer.Lexer
 	currToken token.Token
 	peekToken token.Token
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{
 		l: l,
+		errors: []string{},
 	}
 	p.nextToken()
 	p.nextToken()
@@ -48,4 +50,50 @@ func (p *Parser) parseStatement() ast.Statement {
 	default:
 		return nil
 	}
+}
+
+func (p *Parser) parseLetStatement () *ast.LetStatement {
+	stmt := &ast.LetStatement{Token:p.currToken}
+	if !p.expectPeek(token.IDENT){
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token:p.currToken , Value : p.currToken.Literal}
+	if !p.expectPeek(token.ASSIGN){
+		return nil
+	}
+	//skipping expression for now 
+	
+	for !p.currTokenIs(token.SEMICOLON){
+		p.nextToken()
+	}
+
+	return stmt
+
+}
+
+func (p *Parser) currTokenIs(t token.TokenType) bool {
+	return p.currToken.Type == t
+}
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+	
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p *Parser)Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType){
+	msg := fmt.Sprintf("expected next token to be %s , got %S instead ",t,p.nextToken.Type)
+	p.errors = append(p.errors,msg)
 }
